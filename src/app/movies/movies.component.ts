d41a1b83dc52bef0ee2655e2ad09714021f194d3
C820683 <ngwako.mahasha@standardbank.co.za>
2019-10-04 15:12:08 +0200

    Demo streaming web ng app

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -19,12 +19,6 @@ export class MoviesComponent implements OnInit {
     this.moviesFeed = this.moviesList.entries.map((item: any) => {
       return item;
     });
-    console.log(this.moviesFeed);
-    console.log(this.moviesFeed.map((x) => {
-      if (x.programType === 'movie' && x.length > 21) {
-        return x.title;
-        }
-     }));
   }
 
 }
